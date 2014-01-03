ca91bc0646d6963237e4531688d287d6b233d76b
patrick <patryk@takeid.pl>
2014-01-03 17:18:07 +0100

    remember locale before async task is run

--- lib/devise/async/backend/base.rb
+++ lib/devise/async/backend/base.rb
@@ -11,9 +11,11 @@ module Devise
         # It uses `orm_adapter` API to fetch the record in order to enforce
         # compatibility among diferent ORMs.
         def perform(method, resource_class, resource_id, *args)
-          resource = resource_class.constantize.to_adapter.get!(resource_id)
-          args[-1] = args.last.symbolize_keys if args.last.is_a?(Hash)
-          mailer_class(resource).send(method, resource, *args).deliver
+          I18n.with_locale locale_from_args(args) do
+            resource = resource_class.constantize.to_adapter.get!(resource_id)
+            args[-1] = args.last.symbolize_keys if args.last.is_a?(Hash)
+            mailer_class.send(method, resource, *args).deliver
+          end
         end
 
         private
@@ -21,6 +23,12 @@ module Devise
         def mailer_class(resource = nil)
           @mailer_class ||= resource.try(:devise_mailer) || Devise.mailer
         end
+
+        def locale_from_args(args)
+          args_last = args.last
+          args_last.delete('locale') if args_last.is_a?(Hash)
+        end
+
       end
     end
   end
